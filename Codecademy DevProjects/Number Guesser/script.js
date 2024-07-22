35a39cda253f24741bcbb370ecab58afa8150241
Alex Kiril Casimir Villa <59150525+blatherson@users.noreply.github.com>
2024-07-22 11:47:26 +0000

    Finished a practice project

---
+++ Codecademy DevProjects/Number Guesser/script.js
@@ -0,0 +1,26 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+function generateTarget() {
+  return Math.floor(Math.random() * 10);
+}
+
+function compareGuesses(humanGuess, computerGuess, target) {
+  const humanDifference = Math.abs(target - humanGuess);
+  const computerDifference = Math.abs(target - computerGuess);
+  return humanDifference <= computerDifference;
+}
+
+function updateScore(winner) {
+    if (winner === 'human') {
+        humanScore++;
+    } else if (winner === 'computer') {
+        computerScore++;
+    }
+    }
+
+function advanceRound() {
+    currentRoundNumber++;
+}
+
